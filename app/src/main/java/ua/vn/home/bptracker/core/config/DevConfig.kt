28013f785da6dd3f7a7eaa2efec169d905d823f2
Alexsik76 <o.sikorskyi.vn@gmail.com>
2026-06-14 15:43:34 +0300

    Set MOCK_MODE to false as per implementation instructions

--- app/src/main/java/ua/vn/home/bptracker/core/config/DevConfig.kt
+++ app/src/main/java/ua/vn/home/bptracker/core/config/DevConfig.kt
@@ -3,4 +3,4 @@ package ua.vn.home.bptracker.core.config
 /**
  * Toggle this to true for local UI work on the emulator without backend/passkey.
  */
-const val MOCK_MODE = true
+const val MOCK_MODE = false
