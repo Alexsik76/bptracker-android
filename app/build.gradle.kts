33fbbf2fd8bd722843f7d807921f6d5af9d16137
Alexsik76 <o.sikorskyi.vn@gmail.com>
2026-07-15 09:42:52 +0300

    add dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,17 +18,20 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 2
-        versionName = "1.0.1"
+        versionName = "1.0.1-b"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", "\"https://api2-bptracker.home.vn.ua/\"")
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".dev"
+            manifestPlaceholders["appName"] = "BP Tracker DEV"
+        }
         release {
-            optimization {
-                enable = false
-            }
+            manifestPlaceholders["appName"] = "BP Tracker"
+            isMinifyEnabled = false
         }
     }
     compileOptions {
